b8184317e132673b49cf3f375e2b982aeecfb682
Olaf Wrieden <42084239+olafwrieden@users.noreply.github.com>
2019-09-28 19:06:47 +1200

    colors frontend

--- app.js
+++ app.js
@@ -10,7 +10,8 @@ const app = express();
 const port = process.env.PORT;
 
 app.use(express.json());
-//app.use(express.static("public"));
+app.use(express.static("views"));
+app.set("view engine", "ejs");
 
 const clientId = "hackathon2019";
 const clientSecret = "secret";
@@ -27,7 +28,6 @@ const scopes = ["userinfo", "accounts", "balance", "transactions", "payments"];
  * Default Route, Render Client Page
  */
 app.get("/", (req, res) => {
-  console.log("test");
   const authURL = authClient.generateAuthorizationURL(scopes, "nonce", "state");
   res.redirect(authURL);
 });
@@ -42,12 +42,15 @@ app.get("/callback", async (req, res) => {
   const tokens = await authClient.exchangeToken(code);
   const accessToken = tokens.access_token;
 
+  TOKEN = accessToken;
+
   // return the access token to the client
   res.redirect(`/app?accessToken=${accessToken}`);
 });
 
 app.get("/app", async (req, res) => {
   const accessToken = req.query.accessToken;
+  console.log(accessToken);
   const accounts = await apiClient.getAccounts(accessToken);
 
   let html = "<html>";
@@ -55,7 +58,6 @@ app.get("/app", async (req, res) => {
 
   for (const account of accounts.data) {
     // query 3 months of transactions from the users account
-    console.log("account", account);
     const balances = await apiClient.getAccounts(
       accessToken,
       account.accountId
@@ -67,8 +69,11 @@ app.get("/app", async (req, res) => {
 
   html += "</ul></html>";
 
-  res.send(html);
+  //res.send(html);
   //res.sendFile(__dirname + "/public/index.html");
+  return res.render("page", {
+    accounts: accounts
+  });
 });
 
 /**
@@ -87,10 +92,10 @@ app.get("/dummy-data", (req, res) => {
  */
 app.get("/data", async (req, res) => {
   const id = req.query.accountId;
-  const accessToken = req.query.accessToken;
-  if (!id || !accessToken) {
-    return res.send("No id or token parameter");
-  }
+  const accessToken = req.query.accessToken || TOKEN;
+  // if (!id || !accessToken) {
+  //   return res.send("No id or token parameter");
+  // }
 
   let returnData = {};
 
@@ -98,12 +103,42 @@ app.get("/data", async (req, res) => {
   for (let i = 0; i < accounts.data.length; i++) {
     if (accounts.data[i].accountId === id) {
       Object.assign(returnData, accounts.data[i]);
-      const balances = await apiClient.getBalance(accessToken, id);
+      const balances = await getAccountBalance(accessToken, id);
       Object.assign(returnData, balances.data[0]);
       return res.send(returnData);
     }
   }
   return res.send(`No account with this id: ${id}`);
 });
+
+/**
+ * returns accounts data
+ */
+app.get("/accounts", async (req, res) => {
+  const accessToken = req.query.accessToken;
+  let accounts = await apiClient.getAccounts(accessToken);
+  return accounts ? res.send(accounts.data) : res.send(null);
+});
+
+/**
+ * returns account data by id
+ */
+app.get("/accounts/:id", async (req, res) => {
+  const id = req.params.id;
+  const accessToken = req.query.accessToken || TOKEN;
+  let account = await apiClient.getAccount(accessToken, id);
+  return res.render("main", {
+    account: account
+  });
+  //return account ? res.send(account.data[0]) : res.send(null);
+});
+
+/**
+ * Returns the given account's balance
+ */
+const getAccountBalance = async (accessToken, id) => {
+  let result = await apiClient.getBalance(accessToken, id);
+  return result ? result : null;
+};
 
 app.listen(port, () => console.log(`Herengi API listening on port ${port}!`));
